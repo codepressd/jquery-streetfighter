d0d46d32d76bc4b13a2568cc439fd72ea1e32bc6
Codepressd <creederone@gmail.com>
2016-07-06 08:58:27 -0700

    Ryu firing with sound

--- js/app.js
+++ js/app.js
@@ -5,5 +5,26 @@ $(document).ready(function(){
 	}).mouseleave(function(){
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+	}).mousedown(function(){
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate({'left': '1080px'}, 
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left', '580px');
+			});
+	}).mouseup(function(){
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
 	});
 });
+
+//sound function
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
